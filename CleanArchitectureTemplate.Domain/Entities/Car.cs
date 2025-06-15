8ce742da8602b3f4a7677be897674671f2edc483
Adil H Raza <adilhraza@gmail.com>
2025-06-15 21:54:37 +0100

    API code clean up and adding skeleton projects

---
+++ CleanArchitectureTemplate.Domain/Entities/Car.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanArchitectureTemplate.Domain.Entities;
+
+public class Car
+{
+    public int Id { get; set; }
+    public string Make { get; set; } = string.Empty;
+    public string Model { get; set; } = string.Empty;
+    public int Year { get; set; }
+    public decimal Price { get; set; }
+}
